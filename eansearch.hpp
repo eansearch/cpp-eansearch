4b121081ccb3aa9c9470fb9fdebf8d805e2a1d83
Jan Willamowius <jan@willamowius.de>
2025-12-16 13:39:58 +0100

    language enum

--- eansearch.hpp
+++ eansearch.hpp
@@ -29,18 +29,34 @@ public:
 
 typedef list<Product*> ProductList;
 
+enum Language {
+    English = 1,
+    Danish = 2,
+    German = 3,
+    Spanish = 4,
+    Finish = 5,
+    French = 6,
+    Italian = 8,
+    Dutch = 10,
+    Norwegian = 11,
+    Polish = 12,
+    Portuguese = 13,
+    Swedish = 15,
+    Any = 99
+};
+
 class EANSearch
 {
 public:
 	EANSearch(const string & token);
 
-	ProductFull * BarcodeLookup(const string & ean, int language = 1) const;
+	ProductFull * BarcodeLookup(const string & ean, int language = English) const;
 	ProductFull * IsbnLookup(const string & isbn) const;
 	bool VerifyChecksum(const string & ean) const;
-	ProductList * ProductSearch(const string & name, int only_language = 99, int page = 0) const;
-	ProductList * SimilarProductSearch(const string & name, int only_language = 99, int page = 1) const;
-	ProductList * CategorySearch(int category, const string & name, int only_language = 99, int page = 0) const;
-	ProductList * BarcodePrefixSearch(const string & prefix, int language = 1, int page = 0) const;
+	ProductList * ProductSearch(const string & name, int only_language = Any, int page = 0) const;
+	ProductList * SimilarProductSearch(const string & name, int only_language = Any, int page = 1) const;
+	ProductList * CategorySearch(int category, const string & name, int only_language = Any, int page = 0) const;
+	ProductList * BarcodePrefixSearch(const string & prefix, int language = English, int page = 0) const;
 	string IssuingCountryLookup(const string & ean) const;
 	string BarcodeImage(const string & ean, int width = 102, int height = 50) const;
 
